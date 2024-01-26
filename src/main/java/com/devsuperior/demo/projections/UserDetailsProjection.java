e7624e6eff147c9a1eb2e694252778ff3e61b9be
Diego Alves <djalves424@gmail.com>
2024-01-25 21:51:47 -0300

    Created the RoleDTO, UserDTO and UserDetails interface classes

---
+++ src/main/java/com/devsuperior/demo/projections/UserDetailsProjection.java
@@ -0,0 +1,12 @@
+package com.devsuperior.demo.projections;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+
+    String getPassword();
+
+    Long getRoleId();
+
+    String getAuthority();
+}
