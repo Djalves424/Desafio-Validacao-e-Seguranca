b6fd8148add589e8f734b89dc4d22b232719231e
Diego Alves <djalves424@gmail.com>
2024-01-25 21:56:02 -0300

    Updated DTO, CityDTO and EventDTO classes

--- src/main/java/com/devsuperior/demo/dto/CityDTO.java
+++ src/main/java/com/devsuperior/demo/dto/CityDTO.java
@@ -2,37 +2,41 @@ package com.devsuperior.demo.dto;
 
 import com.devsuperior.demo.entities.City;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class CityDTO {
-	
-	private Long id;
-	private String name;
-	
-	public CityDTO() {
-	}
 
-	public CityDTO(Long id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-	
-	public CityDTO(City entity) {
-		id = entity.getId();
-		name = entity.getName();
-	}
+    private Long id;
 
-	public Long getId() {
-		return id;
-	}
+    @NotBlank(message = "Campo requerido")
+    private String name;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public CityDTO() {
+    }
 
-	public String getName() {
-		return name;
-	}
+    public CityDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public CityDTO(City entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/devsuperior/demo/dto/EventDTO.java
+++ src/main/java/com/devsuperior/demo/dto/EventDTO.java
@@ -3,13 +3,19 @@ package com.devsuperior.demo.dto;
 import java.time.LocalDate;
 
 import com.devsuperior.demo.entities.Event;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public class EventDTO {
 	
 	private Long id;
+	@NotBlank(message = "Campo requerido")
 	private String name;
+	@FutureOrPresent(message = "A data do evento não pode ser passada")
 	private LocalDate date;
 	private String url;
+	@NotNull(message = "Campo requerido")
 	private Long cityId;
 	
 	public EventDTO() {
