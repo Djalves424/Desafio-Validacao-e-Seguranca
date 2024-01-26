539ea3e4d5fe090303599a266f2fa92e11c60608
Diego Alves <djalves424@gmail.com>
2024-01-25 21:48:47 -0300

    Created error handling classes

---
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -0,0 +1,36 @@
+package com.devsuperior.demo.controllers;
+
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.services.EventService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/events")
+public class EventController {
+
+    @Autowired
+    private EventService service;
+
+    @GetMapping
+    public ResponseEntity<Page<EventDTO>> findAll(Pageable pageable) {
+        Page<EventDTO> list = service.findAllPaged(pageable);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+    @PostMapping
+    public ResponseEntity<EventDTO> insert(@Valid @RequestBody EventDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+}
